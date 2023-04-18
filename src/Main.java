ecab05e57b726473f2feed61e409a8057666c7a8
GungdeWiradana <gungde1967@gmail.com>
2023-04-18 17:37:07 +0800

    upload file admin,menu,order,restaurant

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) throws Exception {
+        Tugas_PBO.Login user_login = new Tugas_PBO.Login();
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("=============================");
+        System.out.println("Masukkan Username Untuk Login");
+        System.out.print("Masukkan UserName Anda : ");
+        String user = bf.readLine();
+        System.out.print("Masukkan Password Anda : ");
+        String password = bf.readLine();
+        System.out.println("=============================");
+        if (user.equals(user_login.getUser()) && password.equals(user_login.getPassword_user()) || user.equals(user_login.getAdmin()) && password.equals(user_login.getPassword_admin())) {
+            if (user.equals(user_login.getUser())) {
+                System.out.println("Selamat datang User");
+            } else if (user.equals(user_login.getAdmin())) {
+                System.out.println("Selamat datang Admin");
+            }
+        }
+
+    }
+}
