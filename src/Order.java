0baf4b620c618020a6f5caeb795e918be6c60877
GungdeWiradana <gungde1967@gmail.com>
2023-04-18 17:41:20 +0800

    upload admin,order,menu,restaurant

---
+++ src/Order.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Order {
+
+
+    static void MenuOrder(){
+        System.out.println("======== ORDER MENU ========");
+        System.out.println("| 1. Lihat Restoran         ");
+        System.out.println("| 2. Logout                 ");
+        System.out.println("============================");
+    }
+
+
+}
