8c0ccc80f745ed733b6aed0985459129cc8358b7
GungdeWiradana <gungde1967@gmail.com>
2023-04-18 19:06:38 +0800

    update tugas PBO Restaurant Makan

--- src/Login.java
+++ src/Login.java
@@ -1,52 +1,37 @@
 public class Login {
-    private String user;
-    private String password_user;
-    private String admin;
-    private String password_admin;
-
-    public Login() {
-        this.user = "GungdeUser";
-        this.password_user = "Gungde";
-        this.admin = "GungdeAdmin";
-        this.password_admin = "Gungde";
+    private String username_user,password_user,
+            username_admin,password_admin;
+    public Login(){
+        username_user = "gungdeuser";
+        password_user = "gungde";
+        username_admin = "gungdeadmin";
+        password_admin = "gungde";
     }
 
-    public Login(String user, String password_user, String admin, String password_admin) {
-        this.user = user;
+    public void setUsername_user(String username_user){
+        this.username_user = username_user;
+    }
+    public void setPassword_user(String password_user){
         this.password_user = password_user;
-        this.admin = admin;
+    }
+    public void setUsername_admin(String username_admin){
+        this.username_admin = username_admin;
+    }
+    public void setPassword_admin(String password_admin){
         this.password_admin = password_admin;
     }
 
-    public void setUser(String user) {
-        this.user = user;
+    //getter
+    public String getUsername_user() {
+        return username_user;
     }
-
-    public void setPassword_user(String password_user) {
-        this.password_user = password_user;
-    }
-
-    public void setAdmin(String admin) {
-        this.admin = admin;
-    }
-
-    public void setPassword_admin(String password_admin) {
-        this.password_admin = password_admin;
-    }
-
-    public String getUser() {
-        return this.user;
-    }
-
     public String getPassword_user() {
-        return this.password_user;
+        return password_user;
     }
-
-    public String getAdmin() {
-        return this.admin;
+    public String getUsername_admin() {
+        return username_admin;
     }
-
     public String getPassword_admin() {
-        return this.password_admin;
+        return password_admin;
     }
 }
--- src/Order.java
+++ src/Order.java
@@ -1,14 +1,50 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Order {
 
+    private Restaurant restaurant;
+    private HashMap<Menu, Integer> menus;
+    private int distance;
 
-    static void MenuOrder(){
-        System.out.println("======== ORDER MENU ========");
-        System.out.println("| 1. Lihat Restoran         ");
-        System.out.println("| 2. Logout                 ");
-        System.out.println("============================");
+    public Order(Restaurant restaurant) {
+        this.restaurant = restaurant;
+        this.menus = new HashMap<>();
+    }
+
+    public Restaurant getRestaurant() {
+        return this.restaurant;
+    }
+
+    public HashMap<Menu, Integer> getMenus() {
+        return this.menus;
+    }
+
+    public int getDistance() {
+        return this.distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (Menu menu : this.menus.keySet()) {
+            totalPrice += menu.getHarga() * this.menus.get(menu);
+        }
+        totalPrice += this.distance * 1000;
+        return totalPrice;
+    }
+
+    public void addMenu(Menu menu, int quantity) {
+        if (this.menus.containsKey(menu)) {
+            int currentQuantity = this.menus.get(menu);
+            this.menus.put(menu, currentQuantity + quantity);
+        } else {
+            this.menus.put(menu, quantity);
+        }
     }
 
 
